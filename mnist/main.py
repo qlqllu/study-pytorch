025def0a6965ca3c9acda38e53987ab643a01912
Junshan Liu <junshan_liu@esri.com>
2023-02-21 22:53:34 +0800

    mnist

---
+++ mnist/main.py
@@ -0,0 +1,20 @@
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Download training data from open datasets.
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
